5ca4069d68c0630a67d5285fe1b7e152607bd74d
Elizaveta <kirileneliza@gmail.com>
2025-01-20 02:07:58 +0300

    new function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -14,3 +14,14 @@ def filter_by_state(list_transactions: list, state: str = "EXECUTED") -> Union[l
         if transaction.get("state") == state:
             new_list.append(transaction)
     return new_list
+
+
+def sort_by_date(list_transactions: list, ascending: bool = True) -> Union[list]:
+    """
+    Функция возвращающая новый список словарей, отсортированный по дате.
+    :param list_transactions:
+    :param ascending:
+    :return:
+    """
+    sort_new_list = sorted(list_transactions, key=lambda x: x.get("date"), reverse=ascending)
+    return sort_new_list
