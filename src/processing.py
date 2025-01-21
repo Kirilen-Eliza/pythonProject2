e79dadccddd6ff1277a139c66b93a1db30f5407a
Elizaveta <kirileneliza@gmail.com>
2025-01-22 01:48:48 +0300

    Обновление всех модулей после проверки линтеров

--- src/processing.py
+++ src/processing.py
@@ -1,27 +1,28 @@
 from typing import Union
 
 
-def filter_by_state(list_transactions: list, state: str = "EXECUTED") -> Union[list]:
+def filter_by_state(transactions: list, state: str = "EXECUTED") -> Union[list]:
     """
-    Функция возвращает новый список словарей, содержащий только те словари,
-    у которых ключ state соответствует указанному значению.
-    :param list_transactions: Список словарей с транзакциями.
+    Функция принимает список словарей и опционально значение для ключа
+    state(по умолчанию 'EXECUTED'). Функция возвращает новый список словарей,
+    содержащий только те словари, у которых ключ state соответствует указанному значению.
+    :param transactions: Список словарей с транзакциями.
     :param state: Значение ключа "state" по умолчанию.
     :return:
     """
     new_list = []
-    for transaction in list_transactions:
+    for transaction in transactions:
         if transaction.get("state") == state:
             new_list.append(transaction)
     return new_list
 
 
-def sort_by_date(list_transactions: list, ascending: bool = True) -> Union[list]:
+def sort_by_date(transactions: list, ascending: bool = True) -> Union[list]:
     """
     Функция возвращающая новый список словарей, отсортированный по дате.
-    :param list_transactions:
+    :param transactions:
     :param ascending:
     :return:
     """
-    sort_new_list = sorted(list_transactions, key=lambda x: x.get("date"), reverse=ascending)
-    return sort_new_list
+    sort_list = sorted(transactions, key=lambda x: x.get("date"), reverse=ascending)
+    return sort_list
