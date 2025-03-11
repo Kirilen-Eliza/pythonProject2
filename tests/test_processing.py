1ade8a6e8734cc7b7c0a4c36f624e61f14e52397
Elizaveta <kirileneliza@gmail.com>
2025-03-11 21:08:59 +0300

    updating the tests

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,55 +1,60 @@
 import pytest
 
-
 from src.processing import filter_by_state, sort_by_date
 
 
-def test_filter_by_state(transactions):
+def test_filter_by_default_state(transactions):
     """Функция-тест, тестирует список словарей по заданному параметру
     по умолчанию 'state'."""
-    assert filter_by_state(transactions) == [{'id': 41428829, 'state': 'EXECUTED',
-                                              'date': '2019-07-03T18:35:29.512364'},
-                                             {'id': 939719570, 'state': 'EXECUTED',
-                                              'date': '2018-06-30T02:08:58.425572'}]
+    assert filter_by_state(transactions) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
 
 
-def test_filter_by_state_2(transactions):
+def test_filter_by_non_default_state(transactions):
     """Функция-тест, тестирует вывод списков словарей по заданному параметру
     при вызове функции."""
-    assert filter_by_state(transactions, 'CANCELED') == [{'id': 594226727, 'state': 'CANCELED',
-                                                          'date': '2018-09-12T21:27:25.241689'},
-                                                         {'id': 615064591, 'state': 'CANCELED',
-                                                          'date': '2018-10-14T08:21:33.419441'}]
+    assert filter_by_state(transactions, "CANCELED") == [
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
 
 
-def test_filter_by_state_lack_of_status(transactions):
-    """Функция-тест, проверяет обработку случаев, когда в списке
-    отсутствует словарь с указанным статусом."""
-    with pytest.raises(ValueError):
-        assert filter_by_state(transactions, 'TRALALA')
+@pytest.mark.parametrize('value, expected', [
+    (
+            [{}], []
+    ),
+    (
+            [{'id': 939719570, 'date': '2018-06-30T02:08:58.425572'}], []
+    )
+])
+def test_filter_by_state_wrong_value(value, expected):
+    """Функция-тест, проверяет пустые входные данные"""
+    assert filter_by_state(value) == expected
 
 
-def test_sort_by_date(transactions):
+def test_sort_by_default_date(transactions):
     """Функция-тест, тестирует обработку сортировки по дате по-умолчанию."""
-    assert sort_by_date(transactions) == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                                          {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-                                          {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                                          {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+    assert sort_by_date(transactions) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
 
 
-def test_sort_by_date_false(transactions):
+def test_sort_by_non_default_date_(transactions):
     """Функция-тест, тестирует обработку сортировки по противоположному направлению от умолчания."""
-    assert sort_by_date(transactions, False) == [{'id': 939719570, 'state': 'EXECUTED',
-                                                  'date': '2018-06-30T02:08:58.425572'},
-                                                 {'id': 594226727, 'state': 'CANCELED',
-                                                  'date': '2018-09-12T21:27:25.241689'},
-                                                 {'id': 615064591, 'state': 'CANCELED',
-                                                  'date': '2018-10-14T08:21:33.419441'},
-                                                 {'id': 41428829, 'state': 'EXECUTED',
-                                                  'date': '2019-07-03T18:35:29.512364'}]
+    assert sort_by_date(transactions, False) == [
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    ]
 
 
-def test_sort_by_date_type():
+def test_sort_by_date_type_wrong_value():
     """Функция-тест, обрабатывает исключение, когда дата в словаре не строка."""
     with pytest.raises(TypeError):
-        assert sort_by_date([{'id': 594226727, 'state': 'EXECUTED', 'date': 20180912}])
+        assert sort_by_date([{"id": 594226727, "state": "EXECUTED", "date": 20180912}])
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,17 +1,20 @@
 import pytest
 
+from src.widget import get_date, mask_account_card
 
-from src.widget import mask_account_card, get_date
 
-
-@pytest.mark.parametrize("card_number_and_name, masked_number",
-                         [('Maestro 1596837868705199', 'Maestro 1596 83** **** 5199'),
-                          ('Счет 73654108430135874305', 'Счет **4305'),
-                          ('MasterCard 7158300734726758', 'MasterCard 7158 30** **** 6758'),
-                          ('Visa Classic 6831982476737658', 'Visa Classic 6831 98** **** 7658'),
-                          ('Visa Platinum 8990922113665229', 'Visa Platinum 8990 92** **** 5229'),
-                          ('Visa Gold 5999414228426353', 'Visa Gold 5999 41** **** 6353')])
-def test_mask_account_card(card_number_and_name, masked_number):
+@pytest.mark.parametrize(
+    "card_number_and_name, masked_number",
+    [
+        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+        ("Счет 73654108430135874305", "Счет **4305"),
+        ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+        ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
+        ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
+        ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
+    ],
+)
+def test_mask_account_card_with_different_data(card_number_and_name, masked_number):
     """Функция-тест, с помощью параметризации, проверяет корректность
     применения нужного типа маскировки входных данных."""
     assert mask_account_card(card_number_and_name) == masked_number
