1b236b4b53013dba2f43071224d98c960e88736b
Elizaveta <kirileneliza@gmail.com>
2025-05-05 22:06:29 +0300

    update type annotations

--- src/decorators.py
+++ src/decorators.py
@@ -1,25 +1,29 @@
 from functools import wraps
+from typing import Callable, Optional
 
 
-def log(filename=None):
+def log(filename: Optional[str] = None) -> Callable:
     """
     Декоратор, который записывает в файл или в консоль статус выполнения функции"""
-    def decorator(func):
+
+    def decorator(func: Callable) -> Callable:
         @wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args: Optional[int], **kwargs: Optional[int]) -> Optional[int | None]:
             result = None
             try:
                 result = func(*args, **kwargs)
             except Exception as e:
-                message = f'{func.__name__} error: {e}. Inputs: {args, kwargs}'
+                message = f"{func.__name__} error: {e}. Inputs: {args, kwargs}"
             else:
-                message = f'{func.__name__} ok'
+                message = f"{func.__name__} ok"
             finally:
                 if filename:
-                    with open(filename, 'w', encoding='utf-8') as file:
+                    with open(filename, "w", encoding="utf-8") as file:
                         file.write(message)
                 else:
                     print(message)
                 return result
+
         return wrapper
+
     return decorator
