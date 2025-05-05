310df50dad9a6f2ea257d72461a761361a10ffcc
Elizaveta <kirileneliza@gmail.com>
2025-05-05 22:25:24 +0300

    update test_decorators.py

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -10,11 +10,11 @@ def test_log_output_to_the_console(capsys):
 
     my_function(10, 5)
     result = capsys.readouterr()
-    assert result.out == 'my_function ok\n'
+    assert result.out == "my_function ok\n"
     my_function(10, 0)
     result_not_positive = capsys.readouterr()
-    assert (result_not_positive.out ==
-            "my_function error: division by zero. Inputs: ((10, 0), {})\n")
+    assert result_not_positive.out == "my_function error: division by zero. Inputs: ((10, 0), {})\n"
+
 
 def test_log_file():
     @log("mylog.txt")
@@ -22,16 +22,17 @@ def test_log_file():
         return x / y
 
     my_function(10, 5)
-    with open('mylog.txt', 'r', encoding='utf-8') as file:
+    with open("mylog.txt", "r", encoding="utf-8") as file:
         result_positive = file.read()
-        assert result_positive == 'my_function ok'
+        assert result_positive == "my_function ok"
     my_function(10, 0)
-    with open('mylog.txt', 'r', encoding='utf-8') as file:
+    with open("mylog.txt", "r", encoding="utf-8") as file:
         result_not_positive = file.read()
-        assert result_not_positive == 'my_function error: division by zero. Inputs: ((10, 0), {})'
-    my_function('10',5) # type: ignore[arg-type]
-    with open('mylog.txt', 'r', encoding='utf-8') as file:
+        assert result_not_positive == "my_function error: division by zero. Inputs: ((10, 0), {})"
+    my_function("10", 5)  # type: ignore[arg-type]
+    with open("mylog.txt", "r", encoding="utf-8") as file:
         result_unsupported_type = file.read()
-        assert result_unsupported_type == ("my_function error: unsupported operand"
-                                           " type(s) for /: 'str' and 'int'. Inputs: (('10', 5), {})")
-    os.remove('mylog.txt')
+        assert result_unsupported_type == (
+            "my_function error: unsupported operand" " type(s) for /: 'str' and 'int'. Inputs: (('10', 5), {})"
+        )
+    os.remove("mylog.txt")
